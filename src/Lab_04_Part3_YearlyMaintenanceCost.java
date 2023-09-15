34c6d10e20532936e2b9c607e4b283d72f817a61
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-09-15 09:46:13 -0400

    blah

--- src/Lab_04_Part3_YearlyMaintenanceCost.java
+++ src/Lab_04_Part3_YearlyMaintenanceCost.java
@@ -1,5 +1,35 @@
-public class Main {
+import java.util.Scanner;
+
+public class Lab_04_Part3_YearlyMaintenanceCost {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        double doubleYearly;
+        double stringWinter;
+        double stringSpring;
+        double stringSummer;
+        double stringFall;
+
+        System.out.println("Please enter your maintenance cost for Winter.");
+        double doubleWinter = scan.nextDouble();
+
+        System.out.println("Your Winter maintenance cost is " + doubleWinter + ".");
+
+
+        System.out.println("Please enter your maintenance cost for Spring.");
+        double doubleSpring = scan.nextDouble();
+
+
+        System.out.println("Please enter your maintenance cost for Summer.");
+        double doubleSummer = scan.nextDouble();
+
+
+        System.out.println("Please enter your maintenance cost for Fall.");
+        double doubleFall = scan.nextDouble();
+
+
+        System.out.println("The total yearly cost for your maintenance is $" + doubleYearly + ".");
+        double doubleYearly = scan.nextDouble();
+
     }
 }
